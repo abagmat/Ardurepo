084c4011d2288e565f8cfc4718b4b444f0a47f90
abagmat <14089768+abagmat@users.noreply.github.com>
2025-08-26 19:49:12 +0300

    Update Обновить.py

--- Обновить.py
+++ Обновить.py
@@ -4,10 +4,6 @@ import sys
 code = '''
 import subprocess
 subprocess.call(["git", "pull"]);
-
-subprocess.call(["pip", "install", "urllib3==1.26.6"]);
-subprocess.call(["pip", "install", "pywin32==308"]);
-subprocess.call(["pip", "install", "requests==2.31.0"]);
 '''
 
 if __name__ == '__main__':
